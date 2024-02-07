797d4317d4bcf2ccd79c64ee5b3e9849b81aa046
Andrey M <tolkoigri@yandex.ru>
2024-02-07 19:55:31 +0300

    Выполнен второй шаг проекта.
    Скрипт gendiff.py модифицирован с добавлением в справку информации о возможности выбора формата.

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,8 @@ def main():
         )
     parser.add_argument('first_file', metavar='first_file')
     parser.add_argument('second_file', metavar='second_file')
+    parser.add_argument('-f', '--format', metavar='FORMAT',
+                        help='set format of output')
     args = parser.parse_args()
 
 
