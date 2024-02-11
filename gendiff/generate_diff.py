6aa6b1434f660ce98eb89544989ff659cc145c79
Andrey M <tolkoigri@yandex.ru>
2024-02-11 03:06:33 +0300

    Выполнен пятый шаг проекта.
    1) Добавлена возможность сравнивать YAML-файлы.
    2) Реализована проверка на формат файла, а также на пустоту и существование файла, в связи с чем добавлено несколько тестов.

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import json
+import yaml
+import os
+import sys
 
 
-def tf_to_json(item):
+def values_format(item):
     if item is True:
         return 'true'
     if item is False:
@@ -10,9 +13,30 @@ def tf_to_json(item):
     return item
 
 
+def format_opening(path):
+    # Checking for existence of file
+    try:
+        _ = open(path)
+    except FileNotFoundError:
+        print("The file doesn't exist.")
+        sys.exit()
+    # Checking for emptiness of file
+    if os.stat(path).st_size == 0:
+        return {}
+    # Checking for format of file
+    if path.endswith('.yml') or path.endswith('.yaml'):
+        opened_file = yaml.load(open(path, 'r'), Loader=yaml.SafeLoader)
+    elif path.endswith('.json'):
+        opened_file = json.load(open(path, 'r'))
+    else:
+        print('Unknown format of file!')
+        sys.exit(0)
+
+    return opened_file
+
+
 def generate_diff(path1, path2):
-    file1 = json.load(open(path1, 'r'))
-    file2 = json.load(open(path2, 'r'))
+    file1, file2 = format_opening(path1), format_opening(path2)
 
     items1 = sorted(file1.items())
     items2 = sorted(file2.items())
@@ -21,15 +45,15 @@ def generate_diff(path1, path2):
     for item in items1:
         if file2.get(item[0]) is not None:
             if file1[item[0]] == file2[item[0]]:
-                output += f'    {item[0]}: {tf_to_json(item[1])}\n'
+                output += f'    {item[0]}: {values_format(item[1])}\n'
             else:
-                output += f'  - {item[0]}: {tf_to_json(item[1])}\n'
-                output += f'  + {item[0]}: {tf_to_json(file2[item[0]])}\n'
+                output += f'  - {item[0]}: {values_format(item[1])}\n'
+                output += f'  + {item[0]}: {values_format(file2[item[0]])}\n'
         else:
-            output += f'  - {item[0]}: {tf_to_json(item[1])}\n'
+            output += f'  - {item[0]}: {values_format(item[1])}\n'
 
     for item in items2:
         if file1.get(item[0]) is None:
-            output += f'  + {item[0]}: {tf_to_json(item[1])}\n'
+            output += f'  + {item[0]}: {values_format(item[1])}\n'
 
     return output + '}'
--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -2,17 +2,9 @@ from gendiff.generate_diff import generate_diff
 import pytest
 
 
-@pytest.fixture
-def file1():
-    return './gendiff/files/file1.json'
-
-
-@pytest.fixture
-def file2():
-    return './gendiff/files/file2.json'
-
-def test_diff(file1, file2):
-    result = generate_diff(file1, file2)
+def test_diff_json():
+    result = generate_diff('./tests/fixtures/test1.json',
+                           './tests/fixtures/test2.json')
     assert result == '''{
   - follow: false
     host: hexlet.io
@@ -21,3 +13,67 @@ def test_diff(file1, file2):
   + timeout: 20
   + verbose: true
 }'''
+
+
+def test_diff_yml():
+    result = generate_diff('./tests/fixtures/test1.yml',
+                           './tests/fixtures/test2.yml')
+    assert result == '''{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
+
+
+def test_with_empty_file():
+    result_json = generate_diff('./tests/fixtures/test1.json',
+                           './tests/fixtures/empty.json')
+    result_yml = generate_diff('./tests/fixtures/empty.yml',
+                           './tests/fixtures/test2.yml')
+    assert result_json == '''{
+  - follow: false
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+}'''
+
+    assert result_yml == '''{
+  + host: hexlet.io
+  + timeout: 20
+  + verbose: true
+}'''
+
+
+def test_identical_files():
+    result_json = generate_diff('./tests/fixtures/test1.json',
+                           './tests/fixtures/test1.json')
+    result_yml = generate_diff('./tests/fixtures/test1.yml',
+                           './tests/fixtures/test1.yml')
+    assert result_json == '''{
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
+}'''
+
+    assert result_yml == '''{
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
+}'''
+
+
+def test_format():
+    with pytest.raises(SystemExit) as ex:
+        generate_diff('./err.txt',
+                      './tests/fixtures/test1.json')
+    assert ex.type == SystemExit
+    
+    with pytest.raises(SystemExit) as ex:
+        generate_diff('./tests/fixtures/test1.yml',
+                      './err.rar')
+    assert ex.type == SystemExit
